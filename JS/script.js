8ccb7e47375b2335afe919708ec98e4c816aa573
vladimirochek <volodymyr.rutetskyi@ukr.net>
2021-09-29 13:39:36 +0300

    module pattern example created

--- JS/script.js
+++ JS/script.js
@@ -55,3 +55,32 @@ const dog = {
 englishman.speak()
 ukrainian.speak()
 dog.speak()
+
+// module patters
+
+function BankCard() {
+    const owner =  "Volodymyr"
+    const bank = "Monobank"
+    const cardNumber = "0880 0660 0440 0220"
+    let PIN = 1111
+    let balance = 840
+    
+    return {
+        withdraw: function(props) {
+            if(balance > props.amount) {
+                if((props.amount > 50 && PIN == props.PIN) || props.amount <= 50) {
+                    balance -= props.amount
+                    return `Success! Current balance: ${balance}`
+                } else return `Impossible make the operation`
+            }
+        },
+        checkBalance: function(props) {
+            if(props.PIN == PIN) {
+                return `Balance: ${balance}`
+            } else return "Incorrect PIN"
+        }
+    }
+}
+
+const myBankCard = BankCard()
+console.log(myBankCard)
