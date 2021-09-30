de2f22debe22d708267cbe8eea61ac3cff749b3a
vladimirochek <volodymyr.rutetskyi@ukr.net>
2021-09-30 17:46:32 +0300

    (sayer): create view done

--- REACT/Task02/sayer/src/views/Create/Create.js
+++ REACT/Task02/sayer/src/views/Create/Create.js
@@ -1,16 +1,65 @@
 import React, { useState } from "react";
 import "./Create.css";
 import "../../App.css";
-import Header from '../../components/Header'
+import Header from "../../components/Header";
+import { useHistory } from "react-router-dom"
 
-function Create() {
+const btnSt = {
+  border: "none",
+  minHeight: "70px",
+  minWidth: "70px",
+  borderRadius: "50%",
+  margin: "0 20px 0px 20px",
+  color: "white",
+  fontSize: "1.5rem",
+};
+const inputSt = {
+  border: "none",
+  borderBottom: "1px solid black",
+  fontSize: "1.2rem",
+  lineHeight: "2.2rem",
+  width: "85%",
+  margin: "0 20px 0 20px",
+};
+const containerSt = {
+  padding: "20px",
+  height: "180px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+};
+
+function Create(props) {
+  let history = useHistory()
+
+  const [title, setTitle] = React.useState("");
+
+  function btnClickHandler() {
+    props.addElement(title)
+    setTitle("");
+    history.push('/main')
+  }
+
+  function inputChangeHandler(e) {
+    setTitle(e.target.value);
+  }
 
   return (
-      <div>
-        <Header title="Create new item"/>
-          <input type="text" placeholder="New item title..."/>
-          <button>›</button>
+    <div className="create-view">
+      <Header title="Create new item" />
+      <div style={containerSt}>
+        <input
+          style={inputSt}
+          type="text"
+          value={title}
+          onChange={(e) => inputChangeHandler(e)}
+          placeholder="New item title..."
+        />
+        <button style={btnSt} className="App-bg-1" onClick={btnClickHandler}>
+          <div>{">"}</div>
+        </button>
       </div>
+    </div>
   );
 }
 
